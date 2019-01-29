2f099eb285e346b4eee27b31695a389e50f1dbc4
Lashaunna Samuels <LSamuels_local@blueberry.corp.enterprise.local>
2019-01-29 11:44:12 -0500

    minor adjustement to the header

--- src/appStyles.js
+++ src/appStyles.js
@@ -9,12 +9,9 @@ export const AppBg = styled.div`
         'bottom';
 
     background: linear-gradient(to right,#343748, #2e3240);
-    /* width: 100%; */
 
     @media(max-width: 771px) {
         grid-template-rows: 27vh 70vh;
-        /* grid-template-rows: 30vh 70vh; */
-        /* width: 100%; */
     }
 
 `;
