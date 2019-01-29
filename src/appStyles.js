87954b91f80610d779b9170926c1f6b23811328b
Lashaunna Samuels <LSamuels_local@blueberry.corp.enterprise.local>
2019-01-29 11:22:53 -0500

    body width stle change

--- src/appStyles.js
+++ src/appStyles.js
@@ -3,16 +3,15 @@ import styled from 'styled-components';
 export const AppBg = styled.div`
     display: grid;
     grid-template-rows: 30vh 70vh;
-    grid-template-areas: 
+    grid-template-areas:
         'top'
         'bottom';
-    
+
     background: linear-gradient(to right,#343748, #2e3240);
-    width: 100vw;
+    width: 100%;
 
     @media(max-width: 771px) {
         grid-template-rows: 27vh 70vh;
-        width: 115vw;
     }
-    
+
 `;
